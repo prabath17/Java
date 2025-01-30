49b3a8223da3ce898466c88069d51b337e5f1964
prabath17 <136706003+prabath17@users.noreply.github.com>
2025-01-30 21:10:41 +0530

    Add files via upload

---
+++ Java_files/switchvowels.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class switchvowels{
+public static void main(String args[]){
+String a="hello";
+for (int i=0;i<a.length();i++){
+char b=a.charAt(i);
+switch(b){
+case 'a':
+System.out.println("a is there");
+break;
+case 'e':
+System.out.println("e is there");
+break;
+case 'i':
+System.out.println("i is there");
+break;
+case 'o':
+System.out.println("o is there");
+break;
+case 'u':
+System.out.println("u is there");
+break;
+}
+}
+
+
+
+}}
